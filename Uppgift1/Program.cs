27dd2849de716d6b25cc62dfccf955f1fb8244b1
Tajton <mattias.larsen@edu.huddinge.se>
2017-09-04 17:22:14 +0200

    Blackjack Uppgift

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int kort = 0;
+            int suit = 0;
+            
+
+            String[] kortlek = new String[52];
+            
+
+
+            for (int i = 0; i < 52; i++)
+            {
+                kort = rand.Next(1, 14);
+                suit = rand.Next(1, 5);
+
+                if (!kortlek.Contains(kort.ToString() + ' ' + suit.ToString()))
+                {
+                    
+
+                    kortlek[i] = suit.ToString().Replace("1", "Hjärter").Replace("2", "Klöver")
+                        .Replace("3", "Ruter").Replace("4", "Spader") + ' ' + kort.ToString();
+                }
+
+                Console.WriteLine(kortlek[i]);
+                
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ Uppgift2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int value = 0;
+            int suit = 0;
+
+
+            String[] deck = new String[52];
+
+            
+
+            for (int i = 0; i < 52; i++)
+            {
+                value = rand.Next(1, 14);
+                suit = rand.Next(1, 5);
+
+                if (!deck.Contains(suit.ToString() + ' ' + value.ToString()))
+                {
+
+
+                    deck[i] = suit.ToString().Replace("1", "Hjärter").Replace("2", "Klöver")
+                        .Replace("3", "Ruter").Replace("4", "Spader") + ' ' + value.ToString();
+                }
+            }
+
+            int n = 0;
+            string svar;
+            int cardValue = 0;
+            int pointSum = 0;
+            do
+            {
+                Console.WriteLine("Vill du dra ett kort , ja/nej");
+
+                svar = Console.ReadLine();
+
+                if (svar.ToLower().Contains("ja"))
+                {
+
+                    
+                    string[] cardsDrawn = deck[n].Split(' ');
+                    cardValue = Convert.ToInt32(cardsDrawn[1]);
+                    pointSum += cardValue;
+                    Console.WriteLine("Du drog " + deck[n] + " och din poängsumma är " + pointSum);
+                    n++;
+                    
+                }
+
+            } while (svar.ToLower().Contains("ja"));
+
+        }
+    }
+}
